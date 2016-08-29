2f19493644f52bf36cf21512bfb8e4c4aec14d04
David Thompson <vs@VS.local>
2016-08-29 12:12:04 +0100

    Update with and add simple genre choice

--- app/wit-client.js
+++ app/wit-client.js
@@ -1,46 +1,30 @@
-const Logger = require('node-wit').Logger;
-const levels = require('node-wit').logLevels;
-const Wit = require('node-wit').Wit;
+const {Wit, log} = require('node-wit');
 
 const firstEntityValue = require('../lib/first-entity-value.js');
+const imdb = require('../lib/imdb.js');
 
 module.exports = (ws) => {
   const actions = {
-    say(sessionId, context, message, cb) {
-      ws.send(JSON.stringify({ msg: message, confidence: context.confidence }));
-      cb();
+    send(request, response) {
+      const {sessionId, context, entities} = request;
+      const {text, quickreplies} = response;
+      return new Promise(function(resolve, reject) {
+        console.log('user said...', request.text);
+        console.log('sending...', JSON.stringify(response));
+        ws.send(JSON.stringify({response}));
+        return resolve();
+      });
     },
-    merge(sessionId, context, entities, message, cb) {
-      // TODO: add whole intent to context
-      context.confidence = entities.intent[0].confidence;
-      const show = firstEntityValue(entities, 'show');
-      if (show) {
-        context.show = show;
-      }
-      cb(context);
-    },
-    error(sessionId, context, error) {
-      console.log(error.message);
-    },
-    runTests(sessionId, context, cb) {
-      // TODO: run mmf test
-      context.resultMsg = 'Looks like you are missing a microfilter, you can just get a new one from here: https://accessories.sky.com/catalogue/products/sky-microfilter?irct=sts';
-      cb(context);
-    },
-    findMovieByGenre(sessionId, context, cb) {
-      // TODO search movie db by genre
-      context.movies = [
-        {title: 'Pretty in Pink'},
-        {title: 'The Breakfast Club'},
-        {title: 'Some Kind of Wonderful'},
-        {title: 'Weird Science'}
-        ];
-      cb(context);
+    findMovieByGenre({sessionId, context, text, entities}) {
+      return new Promise((resolve, reject) => {
+        context.movies = imdb.getMoviesForGenre(text);
+        return resolve(context);
+      });
     }
   };
-
-  const logger = new Logger(levels.DEBUG);
-
-  const token = process.env.WIT_API_KEY;
-  return new Wit(token, actions, logger);
+  return new Wit({
+    accessToken: process.env.WIT_API_KEY, 
+    actions, 
+    logger: new log.Logger(log.DEBUG)
+  });
 };
